731df337af9bd9a359b73465994a119c42d184c4
Santiago Quintero de los Rios <squinterodlr@proton.me>
2025-05-12 23:42:42 +0200

    Base implementation of Box class, basically shitty clone of gymnasium Box

---
+++ src/csg/space.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class Box:
+    """A space representing a box in several dimensions"""
+
+    def __init__(self, dim, lower_bounds, upper_bounds):
+        self.dim = dim
+        self.lower_bounds = lower_bounds
+        self.upper_bounds = upper_bounds
+
+    def sample(self, num_samples=1, rng: np.random.Generator = None):
+
+        if rng is None:
+            return np.random.uniform(
+                low=self.lower_bounds, high=self.upper_bounds, size=num_samples
+            )
+        else:
+            return rng.uniform(
+                low=self.lower_bounds, high=self.upper_bounds, size=num_samples
+            )
